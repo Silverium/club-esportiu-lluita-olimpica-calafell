d8ee3b8e387b67ad44a441c0d836f683347f506a
Silverium <soldeplatadeveloper@gmail.com>
2023-09-29 14:08:37 +0200

    Revert "feat(facebook): add facebook plugin"
    This reverts commit f32e7a43f14f9420b044a16131385f0b2b804c9c.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="bg-white text-black">
-        <div id="fb-root"></div>
-        <Script async defer crossOrigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v18.0&appId=311555238858518" nonce="Ops48Xy3"></Script>
         <Main />
         <NextScript />
       </body>

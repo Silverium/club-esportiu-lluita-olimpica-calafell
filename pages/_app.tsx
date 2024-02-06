d2e848725710f515c3008098db779c5bb66bcaa9
Silverium <soldeplatadeveloper@gmail.com>
2024-02-06 08:35:20 +0100

    Update page title in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ export default function App({
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href="/" className={`flex items-center space-x-3 rtl:space-x-reverse 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}>
             <Image src="/favicon/safari-pinned-tab.svg" className="h-8" width="32" height={32} alt="Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">C.E. Lluita Olímpica Calafell</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Lluita Olímpica Calafell</span>
           </Link>
           <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>

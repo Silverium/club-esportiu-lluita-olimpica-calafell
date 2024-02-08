14dac21bc87f0bb396f41a1fca95ea5b8643de09
Silverium <soldeplatadeveloper@gmail.com>
2024-02-08 21:27:55 +0100

    Update package.json and contacte.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-white text-black h-screen">
+      <body className="bg-white text-black h-lvh">
         <Main />
         <NextScript />
       </body>
--- pages/contacte.tsx
+++ pages/contacte.tsx
@@ -1,32 +1,71 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import BlogMeta from 'components/BlogMeta';
+import Head from 'next/head';
+import { FC, useState } from 'react';
 import MapBox, { NavigationControl } from "react-map-gl";
-const BCN_POSITION = {
-	latitude: 41.2034281,
-	longitude: 1.5679,
+import { Marker, Popup } from "react-map-gl";
+const EL_CASTELL_POSITION = {
+    latitude: 41.2034281,
+    longitude: 1.5679,
 }
-import { MapEvent, Marker, MarkerProps, Popup } from "react-map-gl";
 
 const accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 const style = "mapbox://styles/mapbox/streets-v9";
-const ContactePage: React.FC = () => {
+const ContactePage: FC = () => {
+    const [showPopup, setShowPopup] = useState<boolean>(false);
     return (
-        <div className="flex h-dvh flex-col justify-center m-2 md:m-28 md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
+        <div className="h-svh flex flex-col justify-center m-2 md:p-10 md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
+            <Head>
+                <title>Contacte | LLuita Olímpica Calafell</title>
+                <BlogMeta />
+            </Head>
             <div>
-                Email: <a href="mailto:lluitaolimpicacalafell@gmail.com?subject=Informaci%C3%B3.%20%5Borigen%3A%20web%5D&body=Hola%20V%C3%ADctor%21%0A%0A">lluitaolimpicacalafell@gmail.com</a>
+                <div>
+                    <h1 className='font-bold'>La lluita és un esport de contacte... contacta'ns!</h1>
+                    <p>Club Esportiu Lluita Olímpica Calafell</p>
+                    <p>Plaça Cosme Maine</p>
+                    <p>43820 Calafell, Tarragona</p>
+                    <h2 className='font-bold'>Horari</h2>
+                    <p>Dimarts i dijous de 18:00 a 20:00</p>
+                    <p>Dilluns, dimecres i divendres de 19:00 a 20:00</p>
+                </div>
+                <span className='font-bold'>Email:</span> <a href="mailto:lluitaolimpicacalafell@gmail.com?subject=Informaci%C3%B3.%20%5Borigen%3A%20web%5D&body=Hola%20V%C3%ADctor%21%0A%0A">lluitaolimpicacalafell@gmail.com</a>
             </div>
-            <div className="h-[500px] w-full">
+            <div className="h-1/2 md:h-full w-full">
 
-            <MapBox
-                mapboxAccessToken={accessToken}
-                initialViewState={{
-                    ...BCN_POSITION,
-                    zoom: 16,
-                }}
-                mapStyle={style}>
+                <MapBox
+                    mapboxAccessToken={accessToken}
+                    initialViewState={{
+                        ...EL_CASTELL_POSITION,
+                        zoom: 16,
+                    }}
+                    mapStyle={style}>
                     <NavigationControl />
-                    <Marker latitude={BCN_POSITION.latitude} longitude={BCN_POSITION.longitude}>
+                    <Marker latitude={EL_CASTELL_POSITION.latitude} longitude={EL_CASTELL_POSITION.longitude} onClick={ev => {
+                        ev.originalEvent?.stopPropagation();
+                        setShowPopup((prev) => !prev);
+                    }}>
                         <div className="text-4xl">📍</div>
+                        {showPopup && (
+                            <Popup
+                                longitude={EL_CASTELL_POSITION.longitude}
+                                latitude={EL_CASTELL_POSITION.latitude}
+                                anchor="bottom"
+                                offset={20}
+                                closeButton={false}
+                                onClose={() => setShowPopup(false)}
+                            >
+                                <div className='p-1'>
+                                    <h3>Club Esportiu Lluita Olímpica Calafell</h3>
+                                    <p>Plaça Cosme Maine</p>
+                                    <p>43820 Calafell, Tarragona</p>
+                                    <a href='https://www.google.com/maps/dir/?api=1&destination=Escola+Castell+de+Calafell'
+                                        className='text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                                        target="_blank" rel="noopener noreferrer">Com arribar-hi</a>
+                                </div>
+                            </Popup>
+                        )}
                     </Marker>
                 </MapBox>
             </div>

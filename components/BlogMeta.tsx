9f9d6fe3c92fa0198a917c3f8e3d9cd55bfe5764
Soldeplata <soldeplatadeveloper@gmail.com>
2024-03-15 09:51:59 +0100

    Add favicon.ico to BlogMeta.tsx

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -11,6 +11,7 @@ export default function BlogMeta() {
         sizes="180x180"
         href="/favicon/apple-touch-icon.png"
       />
+      <link rel="icon" href="/favicon/favicon.ico" />
       <link
         rel="icon"
         type="image/png"
@@ -24,7 +25,6 @@ export default function BlogMeta() {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />

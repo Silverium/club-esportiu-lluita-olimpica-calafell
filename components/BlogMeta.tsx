5a4c6a1b71ca718904daa6eb438ce47c1dc1d238
Soldeplata <soldeplatadeveloper@gmail.com>
2024-03-27 16:40:37 +0100

    feat(structured data): added to the site as an organization

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -1,3 +1,32 @@
+import Script from "next/script";
+import { Organization, WithContext } from "schema-dts";
+const jsonLd: WithContext<Organization> = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "image": "https://www.lluitacalafell.com/favicon/safari-pinned-tab.svg",
+  "url": process.env.NEXT_PUBLIC_VERCEL_URL ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL : process.env.NODE_ENV === 'production' ? 'https://www.lluitacalafell.com' : '',
+  // "sameAs": ["https://example.net/profile/example1234", "https://example.org/example1234"],
+  "logo": "https://www.lluitacalafell.com/favicon/safari-pinned-tab.svg",
+  "name": "C.E. Lluita Olímpica Calafell",
+  "description": "Entitat esportiva federada a la Federació Catalana de Lluita i a la Federació Espanyola de Lluita, dedicada a la promoció de la lluita olímpica a Calafell, al Baix Penedès i a Catalunya.",
+  "email": "lluitaolimpicacalafell@gmail.com",
+  "telephone": "+34636557181",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Plaça Cosme Maine",
+    "addressLocality": "Calafell",
+    "addressCountry": "ES",
+    "addressRegion": "Tarragona",
+    "postalCode": "43820"
+  },
+  sponsor: {
+    "@type": "Person",
+    "name": "Soldeplata Saketos",
+    "url": "https://www.soldeplata.dev",
+    "description": "Desenvolupador web freelance especialitzat en tecnologies modernes com React, Next.js, Tailwind CSS, Sanity i Vercel.",
+    "email": "soldeplatadeveloper@gmail.com",
+  },
+};
 /**
  * All the shared stuff that goes into <head> on `(blog)` routes, can be be imported by `head.tsx` files in the /app dir or wrapped in a <Head> component in the /pages dir.
  */
@@ -23,6 +52,13 @@ export default function BlogMeta() {
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
       <meta name="theme-color" content="#ffffff" />
+      <Script
+        id="faq-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd),
+        }}
+      />
     </>
   )
 }
